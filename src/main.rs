132e07a0f4a383725c2beb7b4450c6fc46e7c4e3
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-03-20 01:00:57 -0400

    feat: second step

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+const MAX_BUFFER_SIZE: usize = 2048;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,12 +10,35 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                read_stream(&mut stream.try_clone().unwrap());
+                write_stream(
+                    &mut stream.try_clone().unwrap(),
+                    b"HTTP/1.1 200 OK\r\n\r\nHello, world!",
+                );
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn read_stream(stream: &mut TcpStream) {
+    let mut buffer = [0; MAX_BUFFER_SIZE];
+    match stream.read(&mut buffer) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Failed to receive data: {}", e);
+        }
+    }
+}
+
+fn write_stream(stream: &mut TcpStream, data: &[u8]) {
+    match stream.write(data) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Failed to send data: {}", e);
         }
     }
 }
